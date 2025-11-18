92813b4783fcfc940b4da9415d0ec9e52fad92a3
willianminatto <willianminatto27@gmail.com>
2025-11-18 12:51:18 -0300

    first commit

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # Grafana K6 Performance Testing
 
-## GitHub Actions + SonarCloud
-
-[![K6 Performance Testing](https://github.com/ugioni/k6-performance-testing/actions/workflows/node.js.yml/badge.svg)](https://github.com/ugioni/k6-performance-testing/actions/workflows/node.js.yml)
-
-</br>
-
-[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=ugioni_playwright-e2e&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=ugioni_playwright-e2e)
-
-## Getting Started
-
-In order to execute this project you must follow the steps below:
-
-1. Install [Node JS](https://nodejs.org/) (version >= 22.x)
-1. Install [Grafana K6](https://dl.k6.io/msi/k6-latest-amd64.msi)
-1. Run `npm i --save-dev` to install all the project dependencies
-1. Run `npm run ci` to execute the entire test suite
-
-All execution reports can be found in `src/output`.
+http://servicodados.ibge.gov.br/api/v3/noticias/
--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,22 +1,23 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const getContactsDuration = new Trend('get_contacts_duration', true);
+export const rateStatusCodeOK = new Rate('rate_status_code_ok');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_failed: ['rate<0.12'],
+    http_req_duration: ['p(95)<5700'],
+    rate_status_code_ok: ['rate>0.95']
   },
   stages: [
-    { duration: '10s', target: 2 },
-    { duration: '10s', target: 4 },
-    { duration: '10s', target: 6 }
+    { duration: '30s', target: 10 },
+    { duration: '1m', target: 100 },
+    { duration: '1m30s', target: 200 },
+    { duration: '2m', target: 300 }
   ]
 };
 
@@ -28,7 +29,7 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'http://servicodados.ibge.gov.br/api/v3/noticias/';
 
   const params = {
     headers: {
@@ -42,7 +43,7 @@ export default function () {
 
   getContactsDuration.add(res.timings.duration);
 
-  RateContentOK.add(res.status === OK);
+  rateStatusCodeOK.add(res.status === OK);
 
   check(res, {
     'GET Contacts - Status 200': () => res.status === OK
