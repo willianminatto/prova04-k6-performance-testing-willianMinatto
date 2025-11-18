8b40c1d4e0456fe9522f10f29ad9de3ccf05bb89
Willian Minatto <willian.279222@alunosatc.edu.br>
2025-11-18 19:26:56 -0300

    prova

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,51 +1,50 @@
-// import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
-// import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-// import http from 'k6/http';
-// import { check } from 'k6';
-// import { Trend, Rate } from 'k6/metrics';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend, Rate } from 'k6/metrics';
 
-// export const getContactsDuration = new Trend('get_contacts_duration', true);
-// export const rateStatusCodeOK = new Rate('rate_status_code_ok');
+export const getContactsDuration = new Trend('get_contacts_duration', true);
+export const rateStatusCodeOK = new Rate('rate_status_code_ok');
 
-// export const options = {
-//   thresholds: {
-//     http_req_failed: ['rate<0.12'],
-//     http_req_duration: ['p(95)<5700'],
-//     rate_status_code_ok: ['rate>0.95']
-//   },
-//   stages: [
-//     { duration: '30s', target: 10 },
-//     { duration: '1m', target: 100 },
-//     { duration: '1m30s', target: 200 },
-//     { duration: '2m', target: 300 }
-//   ]
-// };
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.25'],
+    http_req_duration: ['p(90)<6800'],
+    rate_status_code_ok: ['rate>0.95']
+  },
 
-// export function handleSummary(data) {
-//   return {
-//     './src/output/index.html': htmlReport(data),
-//     stdout: textSummary(data, { indent: ' ', enableColors: true })
-//   };
-// }
+  stages: [
+    { duration: '30s', target: 7 },
+    { duration: '2m70s', target: 92 }
+  ]
+};
 
-// export default function () {
-//   const baseUrl = 'http://servicodados.ibge.gov.br/api/v3/noticias/';
+export function handleSummary(data) {
+  return {
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
+  };
+}
 
-//   const params = {
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   };
+export default function () {
+  const baseUrl = 'http://servicodados.ibge.gov.br/api/v3/noticias/';
 
-//   const OK = 200;
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-//   const res = http.get(`${baseUrl}`, params);
+  const OK = 200;
 
-//   getContactsDuration.add(res.timings.duration);
+  const res = http.get(`${baseUrl}`, params);
 
-//   rateStatusCodeOK.add(res.status === OK);
+  getContactsDuration.add(res.timings.duration);
 
-//   check(res, {
-//     'GET Contacts - Status 200': () => res.status === OK
-//   });
-// }
+  rateStatusCodeOK.add(res.status === OK);
+
+  check(res, {
+    'GET Contacts - Status 200': () => res.status === OK
+  });
+}
